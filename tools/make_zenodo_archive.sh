681f1e396816e9d1e13a114c3570c188475ead95
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2018-12-03 11:26:44 +0000

    Added short script to generate the Zenodo dataset archive

---
+++ tools/make_zenodo_archive.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh
+
+git archive -o pinsoro.zip --prefix=pinsoro/ HEAD
+cd ..
+ln -s freeplay_sandbox/ pinsoro
+zip -rv freeplay_sandbox/pinsoro.zip pinsoro/data/*/pinsoro-*.csv
+zip -rv freeplay_sandbox/pinsoro.zip pinsoro/data/*/pinsoro-*.json
+rm pinsoro
+cd freeplay_sandbox
