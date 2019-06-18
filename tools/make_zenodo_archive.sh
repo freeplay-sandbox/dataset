bbda6d877b35c155c7bcc975506d387013901df8
Séverin Lemaignan <severin.lemaignan@brl.ac.uk>
2019-06-18 09:59:59 +0100

    Fixed minor issues for the scripts to copy the dataset from one place to the other

--- tools/make_zenodo_archive.sh
+++ tools/make_zenodo_archive.sh
@@ -4,6 +4,6 @@ git archive -o pinsoro.zip --prefix=pinsoro/ HEAD
 cd ..
 ln -s freeplay_sandbox/ pinsoro
 zip -rv freeplay_sandbox/pinsoro.zip pinsoro/data/*/pinsoro-*.csv
-zip -rv freeplay_sandbox/pinsoro.zip pinsoro/data/*/pinsoro-*.json
+zip -rv freeplay_sandbox/pinsoro.zip pinsoro/data/*/*.json
 rm pinsoro
 cd freeplay_sandbox
